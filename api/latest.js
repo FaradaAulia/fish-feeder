7475096273550a0d61cc4f86c331d726ffb5aea5
Farada Aulia <shiro1nya5kali@gmail.com>
2026-06-20 19:13:57 +0700

    debug env

--- api/latest.js
+++ api/latest.js
@@ -1,21 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-)
-
 export default async function handler(req, res) {
 
-  const { data, error } = await supabase
-    .from('sensor_data')
-    .select('*')
-    .order('id', { ascending: false })
-    .limit(1)
+  return res.status(200).json({
+    url: process.env.SUPABASE_URL,
+    keyExists: !!process.env.SUPABASE_ANON_KEY
+  })
 
-  if (error) {
-    return res.status(500).json(error)
-  }
-
-  return res.status(200).json(data[0] || {})
 }
